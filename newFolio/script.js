a86f8b46b0a0761b04f03fbd6e769474e5d6f5b3
LukenProgramming <lukeiannapier@gmail.com>
2024-05-29 16:58:48 +0100

    added whole new section to homepage.htm pretty much done except need to make contactForm functional instead of just for show

--- newFolio/script.js
+++ newFolio/script.js
@@ -13,6 +13,19 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+const observerLeft = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.remove('hiddenLeft');
+        } else {
+            entry.target.classList.add('hiddenLeft');
+        }
+    });
+});
+
+const hiddenLeftElements = document.querySelectorAll('.hiddenLeft');
+hiddenLeftElements.forEach((el) => observerLeft.observe(el));
+
 const fonts = ['SanFranProNormal','coolvetica','ltSaeda'];
 const colours = ['red','orange','yellow','green','blue','violet'];
 const styles = ['normal','italic'];
