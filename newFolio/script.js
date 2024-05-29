dbfadf2c2d6552bf7d9dd7bdff524f2254582c4d
LukenProgramming <lukeiannapier@gmail.com>
2024-05-29 04:40:02 +0100

    added more js to make the responsive text actually responsive. also includes random generation. added stuff to 'contact' page. still needs to be mobile-proofed, will do that tomorrow.

--- newFolio/script.js
+++ newFolio/script.js
@@ -12,3 +12,26 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const fonts = ['SanFranProNormal','coolvetica','ltSaeda'];
+const colours = ['red','orange','yellow','green','blue','violet'];
+const styles = ['normal','italic'];
+const responsiveText = document.getElementById('responsiveText');
+
+responsiveText.onmouseover = () => {
+    let randomColourNum = Math.floor(Math.random() * 6);
+    let randomFontNum = Math.floor(Math.random() * 3);
+    let randomStyleNum = Math.floor(Math.random()*2);
+    responsiveText.style.color = colours[randomColourNum];
+    responsiveText.style.fontStyle = styles[randomStyleNum];
+    responsiveText.style.fontFamily = fonts[randomFontNum];
+}
+
+responsiveText.onmouseout = () => {
+    let randomColourNum = Math.floor(Math.random() * 6);
+    let randomFontNum = Math.floor(Math.random() * 3);
+    let randomStyleNum = Math.floor(Math.random()*2);
+    responsiveText.style.color = colours[randomColourNum];
+    responsiveText.style.fontStyle = styles[randomStyleNum];
+    responsiveText.style.fontFamily = fonts[randomFontNum];
+}
